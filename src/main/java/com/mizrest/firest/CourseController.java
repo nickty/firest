bb7bdffd5f62d3a2d2e4fe6ac8aa5d5a5e7fdd57
Mizanur Rahman <nickty.86@gmail.com>
2024-02-13 23:41:55 +0600

    first api with spring boot

---
+++ src/main/java/com/mizrest/firest/CourseController.java
@@ -0,0 +1,19 @@
+package com.mizrest.firest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retriveAllCourses(){
+        return Arrays.asList(
+            new Course(1, "Learn Spring Boot", "Some description"),
+            new Course(2, "Learn Spring Boot 2", "Some description 2")
+        );
+    }
+}
---
+++ src/test/java/com/mizrest/firest/FirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mizrest.firest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
