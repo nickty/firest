43be75487183a5d6f404d8067ab6c088f7283441
Mizanur Rahman <nickty.86@gmail.com>
2024-02-14 08:48:17 +0600

    adding dev tools

--- src/main/java/com/mizrest/firest/CourseController.java
+++ src/main/java/com/mizrest/firest/CourseController.java
@@ -13,7 +13,11 @@ public class CourseController {
     public List<Course> retriveAllCourses(){
         return Arrays.asList(
             new Course(1, "Learn Spring Boot", "Some description"),
-            new Course(2, "Learn Spring Boot 2", "Some description 2")
+            new Course(2, "Learn Spring Boot 2", "Some description 2"),
+            new Course(3, "Learn Spring Boot 2", "Some description 2"),
+            new Course(4, "Learn Spring Boot 2", "Some description 2"),
+            new Course(5, "Learn Spring Boot 2", "Some description 2"),
+            new Course(6, "Learn Spring Boot 2", "Some description 2")
         );
     }
 }
